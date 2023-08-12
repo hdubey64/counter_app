55214030d8698861dd7920119e411a6eb6b20174
Vishal Dubey <hdubey64@gmail.com>
2023-08-12 14:02:39 +0530

    Conunter App Function and UI Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "./react";
+import { useState } from "react";
 
 function App() {
    const [counter, setCounter] = useState(0);
@@ -7,14 +7,28 @@ function App() {
    const increment = () => {
       setCounter(counter + 1);
    };
+   const decrement = () => {
+      setCounter(counter - 1);
+      if (counter == 0) {
+         alert("It Can not be nagative");
+         setCounter(0);
+      }
+   };
 
    return (
-      <div className="App">
+      <div>
          <header className="App-header">
             <p>Counter App</p>
          </header>
-         <h1>{counter}</h1>
-         <button onClick={increment}>Increment</button>
+         <section>
+            <main>
+               <h1>{counter}</h1>
+               <div>
+                  <button onClick={decrement}>Decrement</button>
+                  <button onClick={increment}>Increment</button>
+               </div>
+            </main>
+         </section>
       </div>
    );
 }
